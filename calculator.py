6e44ff06b0903a750a1325fadfcf2a6848c5b1d8
Shikhar Pandey <smart.shikhar07@gmail.com>
2026-01-28 11:41:51 -0500

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -7,8 +7,12 @@ def multiply(a, b):
 
 a = float(input("Enter first number: "))
 b = float(input("Enter second number: "))
-
-print("Addition:", add(a, b))
+c = float(input("Enter your choicer: "))
+if c==1:
+    print("Addition:", add(a, b))
+elif c==2:
 print("Subtraction:", subtract(a, b))
+elif c==3:
 print("Multiplication:", multiply(a, b))
+
 
